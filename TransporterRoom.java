9b29058881f08d36df5fca41f22e706fe01b19c2
cartelkdot <47113944+cartelkdot@users.noreply.github.com>
2019-04-22 12:44:15 -0400

    Working on transporter room pt 2.

--- TransporterRoom.java
+++ TransporterRoom.java
@@ -9,13 +9,21 @@ public class TransporterRoom extends Room
 {
     // instance variables - replace the example below with your own
     
-
+    
+    public TransporterRoom(){
+    }
+    
     /**
      * Constructor for objects of class TransporterRoom
      */
-    public void TransporterRoom()
+    public  Room getExit(String direction)
     {
         // initialise instance variables
+        return findRandomRoom();
+    }
+    
+    private Room findRandomRoom()
+    {
         
     }
 
